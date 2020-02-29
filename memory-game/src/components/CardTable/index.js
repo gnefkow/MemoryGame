a193893452406e801383395ac97dbc9cecaa61c1
Kyle Becker <gnefkow87@gmail.com>
2020-02-29 09:43:21 -0500

    Created components for the top nav and the CardTable. These components successfully display in React

---
+++ memory-game/src/components/CardTable/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./style.css";
+
+// This file exports both the List and ListItem components
+
+function CardTable() {
+  return (
+    <div className="list-overflow-container">
+      <ul className="list-group">Hello world</ul>
+      <li>Thing 1</li>
+      <li>Thing 1</li>
+      <li>Thing 1</li>
+      <li>Thing 1</li>
+      <li>Thing 1</li>
+      <li>Thing 1</li>
+      <li>Thing 1</li>
+    </div>
+  );
+}
+
+export default CardTable;
+
