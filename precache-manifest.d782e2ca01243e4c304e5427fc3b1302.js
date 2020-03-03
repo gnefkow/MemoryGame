e50b60167e20914ae4e8946fc047c007bf59c40c
Kyle Becker <gnefkow87@gmail.com>
2020-03-02 22:02:54 -0500

    Updates

---
+++ precache-manifest.d782e2ca01243e4c304e5427fc3b1302.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5656bf41e37ce4b766eb3f926934ca6",
+    "url": "/MemoryGame/index.html"
+  },
+  {
+    "revision": "7dc0302d308663ee46fd",
+    "url": "/MemoryGame/static/css/main.6fcbe7d7.chunk.css"
+  },
+  {
+    "revision": "125a2d7955910d3851d5",
+    "url": "/MemoryGame/static/js/2.98a6aa93.chunk.js"
+  },
+  {
+    "revision": "246db22df6ab5fe64816ee11b9fad13d",
+    "url": "/MemoryGame/static/js/2.98a6aa93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dc0302d308663ee46fd",
+    "url": "/MemoryGame/static/js/main.0976cb38.chunk.js"
+  },
+  {
+    "revision": "dd7de4a9526c565e4040",
+    "url": "/MemoryGame/static/js/runtime-main.67059e56.js"
+  }
+]);
